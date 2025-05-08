b84bb09663536577d9d7d75c7080ddb1d8f37c57
Brett <brettgraham@gmail.com>
2025-05-08 08:55:20 -0400

    remove validate

--- romancal/source_catalog/source_catalog_step.py
+++ romancal/source_catalog/source_catalog_step.py
@@ -252,8 +252,6 @@ class SourceCatalogStep(RomanStep):
             self.output_ext = "parquet"
             result = source_catalog_model
 
-        # validate the result to flush out any lazy-loaded contents
-        result.validate()
         return result
 
 
